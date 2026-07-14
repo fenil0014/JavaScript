9a5080712da3a1c83404c669998302dc514892ad
fenil0014 <fenilp1406@gmail.com>
2026-07-14 12:10:56 +0530

    seven code

---
+++ 06_dom/one.js
@@ -0,0 +1,12 @@
+const title = document
+console.log(document);
+
+
+const title = document.querySelector("#title");
+
+title.textContent = "Hello Fenil";
+
+title.style.color = "green";
+const text = document.querySelector(".text");
+
+text.textContent = "DOM is Easy";
