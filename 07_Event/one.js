0e15f6443d63bf1bad79568e8418597fee5eea21
fenil0014 <fenilp1406@gmail.com>
2026-07-20 14:59:31 +0530

    project 7

---
+++ 07_Event/one.js
@@ -0,0 +1,48 @@
+
+// click formula 1 
+// document.getElementById('L').onclick = function () {
+//     alert('alert')
+// }
+
+//imp{
+
+// type, timeStamp, defultPrevented
+// target, toElement, srcElement, currentTarget
+// clientX, clientY, screenX, screenY
+// }
+
+
+// click formula 2
+// document.getElementById ('L').addEventListener('click' , function () {
+//     console.log(e);
+//     alert("alert again")
+// },true)
+
+// document.getElementById('code').addEventListener('click', function (e) {
+//     console.log("clicked inside the ul");
+
+// }, false)
+
+// document.getElementById('python').addEventListener('click', function (e) {
+//     console.log("clicked python");
+// }, false)
+
+// false {
+        // first = clicked py
+        // second = clicked inside the ul 
+        // ( li -> ul )
+// }
+
+document.getElementById('code').addEventListener('click', function (e) {
+    console.log("clicked inside the ul");
+    e.stopPropagation()  // ahiya j rese nichhe nai jay
+}, true)
+
+document.getElementById('python').addEventListener('click', function (e) {
+    console.log("clicked python");
+}, true)
+// True {
+        // first = clicked inside the ul 
+        // second = clicked py
+        // ( ul -> li )
+// }
